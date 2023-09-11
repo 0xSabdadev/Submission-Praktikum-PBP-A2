16a1e11a31c8bda289b0ef6123fd7dc95782a963
Friz650 <115050743+farid650@users.noreply.github.com>
2023-09-11 18:29:48 +0700

    Add files via upload

---
+++ 24060120140043/submission-2/variabel1.php
@@ -0,0 +1,15 @@
+/*
+Nama : Farid Khoirur Rizal
+NIM  : 24060120140043
+Lab  : A2
+*/
+<html>
+<?php
+
+//assign nilai ke variabel
+$a = 15;
+echo 'Variabel a berisi : '.$a.'<br />';
+$a = 'Pemrograman Web dan Internet';
+echo 'Variabel a berisi : '.$a.'<br />';
+?>
+</html>
---
+++ 24060120140043/submission-2/variabel_lokal.php
@@ -0,0 +1,18 @@
+/*
+Nama : Farid Khoirur Rizal
+NIM  : 24060120140043
+Lab  : A2
+*/
+<html>
+<?php
+
+// Define a function
+function diskon( ){
+    $harga = 1000;
+    $harga = 0.2 * $harga;
+   }
+   $harga = 2000;
+   diskon();
+   echo 'harga = '.$harga.'<br />';   
+?>
+</html>
