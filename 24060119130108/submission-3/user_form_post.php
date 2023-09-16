c165d4b5d8a683b4bec7f1139dc12f7e4e589d47
Wafi37 <102354515+Wafi37@users.noreply.github.com>
2023-09-16 20:17:20 +0700

    Add files via upload

---
+++ 24060119130108/submission-3/user_form_post.php
@@ -0,0 +1,93 @@
+<!--Nama : Maulana Aliwafi Haykal
+NIM : 24060119130108 
+Praktikum PBP 3 -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Form Input Siswa PHP</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    </head>
+    <style>
+        .error{
+            color: red;
+        }
+    </style>
+    
+    <body>
+        <div class="card m-5">
+        <div class="card-header">Form Mahasiswa</div>
+        <div class="card-body">
+        <form name="formDataMhs" action="" method="POST">
+            <div class="form-group">
+                <label for="nama">Nama:</label>
+                <input type="text" class="form-control" id="nama" name="nama" maxlength="50">
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
+            <div class="form-group">
+                <label for="kota">Kota/ Kabupaten:</label>
+                <select id="kota" name="kota" class="form-control">
+                    <option value="Semarang">Semarang</option>
+                    <option value="Jakarta">Jakarta</option>
+                    <option value="Bandung">Bandung</option>
+                    <option value="Surabaya">Surabaya</option>
+                </select>
+            </div>
+            <label>Jenis Kelamin:</label>
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="pria">Pria
+                </label>
+            </div>
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="wanita">Wanita
+                </label>
+            </div>
+            <br>
+            <label>Peminatan:</label>
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input type="checkbox" class="form-check-input" name="minat[]" value="coding">Coding
+                </label>
+            </div>
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input type="checkbox" class="form-check-input" name="minat[]" value="ux_design">UX Design
+                </label>
+            </div>
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input type="checkbox" class="form-check-input" name="minat[]" value="data_science">Data Science
+                </label>
+            </div>
+            <br>
+            <!-- submit dan reset -->
+            <button type="submit" class="btn btn-primary" name="submit" value="submit">Submit</button>
+            <button type="reset" class="btn btn-danger">Reset</button>
+        </form>
+        </div></div>
+        <?php
+        if (isset($_POST["submit"])){
+            echo "<h3>Your input:</h3>";
+            echo 'Nama = '.$_POST['nama'].'<br/>';
+            echo 'Email = '.$_POST['email'].'<br/>';
+            echo 'Kota = '.$_POST['kota'].'<br/>';
+            echo 'Jenis Kelamin = '.$_POST['jenis_kelamin'].'<br/>';
+            //echo 'Minat = '.$_GET['minat'].'<br/>';
+
+            if (!empty($_POST['minat'])){
+                $minat = $_POST['minat'];
+                echo 'Peminatan yang dipilih: ';
+                foreach($minat as $minat_item){
+                    echo '<br/>'.$minat_item;
+                }
+            }   
+        }
+        ?>
+    </body>
+</html>
