f12ce119a519508fab84f20f9042240f9ab19011
ZenobiaZenaide <100354736+ZenobiaZenaide@users.noreply.github.com>
2023-09-23 05:42:31 +0700

    penugasan.php

--- 24060121140164/submission-3/penugasan.php
+++ 24060121140164/submission-3/penugasan.php
@@ -29,9 +29,7 @@
     <?php
         error_reporting(E_ALL & -E_WARNING & -E_NOTICE);
         
-        //validasi submit
         if (isset($_POST['submit'])) {
-            //validasi NIS
             $nis = test_input($_POST['NIS']);
             if (empty($nis)) {
                 $error_nis = 'nis harus diisi';
@@ -39,7 +37,6 @@
                 $error_nis = "nis berisi angka";
             }
 
-            //validasi nama
             $nama = test_input($_POST['nama']);
             if (empty($nama)) {
                 $error_nama = 'Nama harus diisi';
@@ -47,13 +44,11 @@
             $error_nama = "Nama hanya dapat berisi huruf dan spasi";
             }
             
-            //validasi jenis kelamin
             $jenis_kelamin = $_POST['jenis_kelamin'];
             if ($jenis_kelamin == "") {
             $error_jenis_kelamin = "Jenis kelamin harus diisi";
             }
 
-            //validasi kelas
             $kelas = $_POST['kelas'];
             $ekstrakurikuler = $_POST['ekstrakurikuler'];
             if ($kelas == '' || $kelas == 'kelas') {
@@ -70,6 +65,7 @@
                 }
             }
         }
+
         function test_input($data)
         {
             $data = trim($data);
@@ -82,34 +78,34 @@
         <div class="card-header">Form Input Siswa</div>
             <div class="card-body">
                 <form id="formDataProduk" method="POST" autocomplete="on" action="">
-                    <!-- NIS -->
                     <div class="form-group">
                         <label for="NIS">NIS:</label>
                             <input type="text" class="form-control" id="NIS" name="NIS" minlength="10" maxlength="10" value="<?php if (isset($nis)) {echo $nis;}?>">
                         <div class="error"><?php if (isset($error_nis)) {echo $error_nis;}?></div>
                     </div>
-                    <br>
-                    <!-- Nama -->
                     <div class="form-group">
                         <label for="nama">Nama:</label>
                             <input type="text" class="form-control" id="nama" name="nama" maxlength="50" value="<?php if (isset($nama)) {echo $nama;}?>">
                         <div class="error"><?php if (isset($error_nama)) {echo $error_nama;}?></div>
-                    <br>
                     </div>
-                    <!-- Jenis Kelamin -->
                     <label>Jenis Kelamin</label>
                     <div class="form-check">
                         <label class="form-check-label">
                             <input type="radio" class="form-check-input" name="jenis_kelamin" value="pria"<?php if (isset($jenis_kelamin) && $jenis_kelamin == "pria") echo "checked";?>>Pria</label>
-                        <div class="error"><?php if (isset($error_jenis_kelamin)) echo $error_jenis_kelamin;?>
+                        <div class="error">
+                            <?php 
+                            if (isset($error_jenis_kelamin)) echo $error_jenis_kelamin;
+                            ?>
                     </div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
                             <input type="radio" class="form-check-input" name="jenis_kelamin" value="wanita"<?php if (isset($jenis_kelamin) && $jenis_kelamin == "wanita") echo "checked";?>>Wanita</label>
-                        <div class="error"><?php if (isset($error_jenis_kelamin)) echo $error_jenis_kelamin;?></div>
+                        <div class="error">
+                            <?php 
+                            if (isset($error_jenis_kelamin)) echo $error_jenis_kelamin;?>
+                            </div>
                     </div>
-                    <br>
                     <div class="form-group">
                     <label for="kelas">Kelas:</label>
                         <select class="form-control" name="kelas" id="kelas" onchange="show()">
@@ -119,26 +115,39 @@
                         </select>
                         <div class="error"><?php if (isset($error_kelas)) {echo $error_kelas;}?></div>
                     </div>
-                    <br>
-
-                    <!-- ekstrakurikuler -->
                     <div id="ekstrakurikuler">
                     <label>Ekstrakurikuler:</label>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" name="ekstrakurikuler[]" value="pramuka"<?php if (isset($ekstrakurikuler) && in_array('pramuka', $ekstrakurikuler)) {echo 'checked';}?>>Pramuka
+                            <input type="checkbox" class="form-check-input" name="ekstrakurikuler[]" value="pramuka"
+                            <?php 
+                            if (isset($ekstrakurikuler) && in_array('pramuka', $ekstrakurikuler)) {echo 'checked';}
+                            ?>
+                            >Pramuka
                         </label>
-                        <div class="error"><?php if (isset($error_ekstrakurikuler)) {echo $error_ekstrakurikuler;}?></div>
+                        <div class="error">
+                            <?php 
+                            if (isset($error_ekstrakurikuler)) {echo $error_ekstrakurikuler;}?>
+                        </div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" name="ekstrakurikuler[]" value="seniTari"<?php if (isset($ekstrakurikuler) && in_array('seniTari', $ekstrakurikuler)) {echo 'checked';}?>>Seni Tari
+                            <input type="checkbox" class="form-check-input" name="ekstrakurikuler[]" value="seniTari"
+                            <?php if (isset($ekstrakurikuler) && in_array('seniTari', $ekstrakurikuler)) {echo 'checked';}?>
+                            >Seni Tari
                         </label>
-                        <div class="error"><?php if (isset($error_ekstrakurikuler)) {echo $error_ekstrakurikuler;}?></div>
+                        <div class="error">
+                            <?php
+                             if (isset($error_ekstrakurikuler)) {echo $error_ekstrakurikuler;}?>
+                        </div>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                            <input type="checkbox" class="form-check-input" name="ekstrakurikuler[]" value="sinematografi"<?php if (isset($ekstrakurikuler) && in_array('sinematografi', $ekstrakurikuler)) {echo 'checked';}?>>Sinematografi
+                            <input type="checkbox" class="form-check-input" name="ekstrakurikuler[]" value="sinematografi"
+                            <?php 
+                            if (isset($ekstrakurikuler) && in_array('sinematografi', $ekstrakurikuler)) {echo 'checked';}
+                            ?>
+                            >Sinematografi
                         </label>
                         <div class="error"><?php if (isset($error_ekstrakurikuler)) {echo $error_ekstrakurikuler;}?></div>
                     </div>
@@ -146,15 +155,17 @@
                         <label class="form-check-label">
                             <input type="checkbox" class="form-check-input" name="ekstrakurikuler[]" value="basket"<?php if (isset($ekstrakurikuler) && in_array('basket', $ekstrakurikuler)) {echo 'checked';}?>>Basket
                         </label>
-                        <div class="error"><?php if (isset($error_ekstrakurikuler)) {echo $error_ekstrakurikuler;}?></div>
+                        <div class="error">
+                            <?php 
+                            if (isset($error_ekstrakurikuler)) {echo $error_ekstrakurikuler;}
+                            ?></div>
                     </div>
                     </div>
-                    <br>
-                    <!-- Button Submit dan Reset -->
-                    <button type="submit" class="btn btn-primary" name="submit" value="submit">Submit</button>
+                    <br />
+ 
+                    <button type="submit" class="btn btn-primary" name="submit" value="submit">Sumbit</button>
                     <button type="reset" class="btn btn-danger">Reset</button>
             </div>
-            <br>
             <?php
                 if (isset($_POST["submit"])) {
                     echo "<h3>Your Input</h3>"; 
