761e6f01372c871d5e52f74fcf096f8b753e768f
Varrel <100354535+Creissant@users.noreply.github.com>
2023-09-24 10:36:40 +0700

    PRAK 4 PBP A2 - KONEKSI PHP DAN MYSQL

---
+++ 24060121130062/submission-4/lib/db_login.php
@@ -0,0 +1,27 @@
+<!-- 
+File      : db_login.php		22/09/23
+Penulis   : Varrel / 24060121130062
+Deskripsi : bagian untuk melakukan connect ke database dan test_input
+-->
+
+<?php
+// TODO 1: Buatlah koneksi dengan database
+$db_host = 'localhost';
+$db_database = 'bookorama';
+$db_username = 'root';
+$db_password = '';
+
+$db = new mysqli($db_host, $db_username, $db_password, $db_database);
+if ($db->connect_errno) {
+    die("Could not connect to the database: <br />" . $db->connect_error);
+}
+// TODO 2: Buatlah function test_input
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
---
+++ 24060121130062/submission-4/view_customer.php
@@ -0,0 +1,67 @@
+<!-- 
+File      : view_customer.php		22/09/23
+Penulis   : Varrel / 24060121130062
+Deskripsi : Halaman yang menampilkan data customer
+-->
+
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
+}
+
+include('./header.php') ?>
+
+
+<div class="card mt-5">
+    <div class="card-header">Customers Data</div>
+    <div class="card-body">
+        <a href="add_customer.php" class="btn btn-primary mb-4">+ Add Customer Data</a>
+        <a href="logout.php" class="btn btn-danger mb-4">Logout</a>
+        <br>
+        <table class="table table-striped">
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>City</th>
+                <th>Action</th>
+            </tr>
+            <?php
+            // TODO 1: Buat koneksi dengan database
+            require_once('./lib/db_login.php');
+
+            // TODO 2: Tulis dan eksekusi query ke database
+            $query = 'SELECT * FROM customers ORDER BY customerid';
+            $result = $db->query($query);
+            if (!$result) {
+                die("Could not query the database: <br />" . $db->error . "<br>Query: " . $query);
+            }
+
+
+            // TODO 3: Parsing data yang diterima dari database ke halaman HTML/PHP
+            $i = 1;
+            while ($row = $result->fetch_object()) {
+                echo '<tr>';
+                echo '<td>' . $i . '</td>';
+                echo '<td>' . $row->name . '</td>';
+                echo '<td>' . $row->address . '</td>';
+                echo '<td>' . $row->city . '</td>';
+                echo '<td><a class="btn btn-warning btn-sm" href="edit_customer.php?id=' . $row->customerid . '">Edit</a>&nbsp;<a class="btn btn-danger btn-sm" href="delete_customer.php?id=' . $row->customerid . '">Delete</a></td>';
+                echo '</tr>';
+                $i++;
+            }
+
+            // TODO 4: Lakukan dealokasi variabel $result
+            echo '</table>';
+            echo '<br />';
+            echo 'Total Rows = ' . $result->num_rows;
+
+            // TODO 5: Tutup koneksi dengan database
+            $result->free();
+            $db->close();
+
+            ?>
+    </div>
+</div>
+<?php include('./footer.php') ?>
