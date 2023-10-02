a4d9f9d0aed69e86dcf972d5c1d01a7144b87dd5
FaiqRindha <100180165+FaiqRindha@users.noreply.github.com>
2023-10-03 02:36:05 +0700

    Add files via upload

---
+++ 24060121130091/submission-5/get_server_time.php
@@ -0,0 +1,11 @@
+<!-- Nama: Fa'iq Rindha Maulana -->
+<!-- NIM: 24060121130091 -->
+<!-- Lab: PBP A2 -->
+<!-- Nama File: get_server_time.php -->
+<!-- Deskripsi File: File untuk mengambil waktu server terkini -->
+<!-- Tanggal Pembuatan: 01/10/2023 -->
+
+<?php
+// TODO 1: Tuliskan response berupa waktu server saat ini
+echo date('H:i:s');
+?>
