c0204e50a29f046129f93a8cfba8039987dd66cb
Friz650 <115050743+farid650@users.noreply.github.com>
2023-10-04 05:57:28 +0700

    Add files via upload

---
+++ 24060120140043/submission-5/search_book.php
@@ -0,0 +1,11 @@
+<?php include('header.html') ?>
+<br>
+<div class="card">
+   <div class="card-header">Search Book</div>
+   <div class="card-body">
+      <input type="text" class="form-control" id="input_user" name="input_user" oninput="showBook()" placeholder="Ketik judul buku">
+      <br>
+      <div id="result_book"></div>
+   </div>
+</div>
+<?php include('footer.html') ?>
