bf9aa0723d2269c0f43cb45346310aba6f3d3986
Al-Farisi <fitra.yudhisaputra@gmail.com>
2023-10-03 21:07:30 +0700

    Add files via upload

---
+++ 24060121140124/submission-5/tugas5_24060121140124/add_customer_post.php
@@ -0,0 +1,34 @@
+<!-- // Nama    : Fitra Syamli Yudhisaputra
+// NIM          : 24060121140124
+// Tanggal      : 3 Oktober 2023
+// File        : add_customer_post.php
+// Deskripsi   : menampilkan form add data customer dan mengupdate data ke database dengan metode post-->
+<?php
+require_once('./lib/db_login.php');
+
+$name = $db->real_escape_string($_POST['name']);
+$address = $db->real_escape_string($_POST['address']);
+$city = $db->real_escape_string($_POST['city']);
+
+// Assign a query
+$query = "INSERT INTO customers (name, address, city) VALUES ('" . $name . "', '" . $address . "', '" . $city . "')";
+$result = $db->query($query);
+
+if (!$result) {
+    // TODO 1: Jika gagal, tulislah response yang sesuai
+    echo '<div class="alert alert-danger alert-dismissible">
+        <strong>Error!</strong> Could not query the database<br>' .
+        $db->error . '<br>query = ' . $query .
+        '</div>';
+} else {
+    // TODO 2: Jika sukses, tulislah response yang sesuai
+    echo '<div class="alert alert-succes alert-dismissible">
+            <strong>Succes!</strong> Data has been added.<br>
+            Name    : ' . $_POST['name'] . '<br>
+            Address : ' . $_POST['address'] . '<br>
+            City    : ' . $_POST['city'] . '<br>
+        </div>';
+}
+
+// Close DB Connection
+$db->close();
---
+++ 24060121140124/submission-5/tugas5_24060121140124/get_server_time.php
@@ -0,0 +1,9 @@
+<!-- // Nama        : Fitra Syamli Yudhisaputra
+// NIM          : 24060121140124
+// Tanggal      : 3 Oktober 2023
+// File        : get_server_time.php
+// Deskripsi   : Mendapatkan waktu server-->
+<?php
+// TODO 1: Tuliskan response berupa waktu server saat ini
+echo date('H:i:s');
+?>
